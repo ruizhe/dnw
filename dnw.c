a6624643aff8b1c99636085322d8e4e39d1ab587
Ruizhe Zhao <ruizhe.z.g@gmail.com>
2011-10-05 20:10:31 +0800

    almost can be use

--- dnw.c
+++ dnw.c
@@ -2,61 +2,109 @@
 #include <stdlib.h>
 #include <string.h>
 #include <signal.h>
+
 #include <libusb.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
-#define CTRL_IN (LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_ENDPOINT_IN)
-#define CTRL_OUT (LIBUSB_REQUEST_TYPE_VENDOR | LIBUSB_ENDPOINT_OUT)
-#define USB_RQ 0x04
-
-static int print_f0_data(struct libusb_device_handle *devh)
+static struct libusb_device_handle *init_s3c2440_usb()
 {
-    unsigned char data[0x10];
     int r;
-    unsigned int i;
-    int wrote = 0;
-
-    memset(data, 0, 0x10);
-    *((unsigned int *)data) = 0x32000000;
-    *((unsigned int *)data + 1) = 0x10;
-
-
-    r = libusb_bulk_transfer(devh, 0x03, data, 0x10, &wrote, 0);
-    if (r < 0) {
-        fprintf(stderr, "F0 error %d\n", r);
-        return r;
-    }
-    printf("%d byte(s) wrote\n", wrote);
-    return r;
-}
-
-int main(void)
-{
-    struct sigaction sigact;
     struct libusb_device_handle *devh = NULL;
-    int r = 1;
 
     r = libusb_init(NULL);
     if (r < 0) {
         fprintf(stderr, "failed to initialise libusb\n");
-        exit(1);
+        return NULL;
     }
 
     devh = libusb_open_device_with_vid_pid(NULL, 0x5345, 0x1234);
     if (devh == NULL) {
         fprintf(stderr, "Could not find/open device\n");
-        goto out;
+        return NULL;
     }
 
     r = libusb_claim_interface(devh, 0);
     if (r < 0) {
         fprintf(stderr, "usb_claim_interface error %d\n", r);
-        goto out;
+        libusb_close(devh);
+        devh = NULL;
     }
 
-    r = print_f0_data(devh);
+    return devh;
+}
 
-out:
-    libusb_close(devh);
+static void close_s3c2440_usb(struct libusb_device_handle *devh)
+{
+    if (devh != NULL)
+        libusb_close(devh);
     libusb_exit(NULL);
-    return r >= 0 ? r : -r;
+}
+
+int main(int argc, char *argv[])
+{
+    struct libusb_device_handle *devh = NULL;
+    int fd = -1;
+    struct stat sb;
+    int count = 0;
+    char *buf;
+    int len;
+    int r;
+    int wrote;
+
+    if (argc < 2) {
+        printf("Usage: %s <filename>\n"
+               "    <filename> : file to download\n", argv[0]);
+        exit(1);
+    }
+    devh = init_s3c2440_usb();
+    if (devh == NULL) {
+        exit (1);
+    }
+    fd = open(argv[1], O_RDONLY);
+    if (fd < 0) {
+        fprintf(stderr, "Can't open file %s\n", argv[1]);
+        goto err;
+    }
+    if (0 > fstat(fd, &sb)) {
+        fprintf(stderr, "Can't stat fd\n");
+        goto err;
+    }
+    buf = (char *)malloc(sb.st_size + 8);
+    if (buf == NULL) {
+        fprintf(stderr, "alloc mem failed\n");
+        goto err;
+    }
+    *((unsigned int *)buf) = 0x32000000;
+    *((unsigned int *)buf + 1) = sb.st_size;
+    if (sb.st_size != read(fd, buf+8, sb.st_size)) {
+        fprintf(stderr, "read file error\n");
+        goto err;
+    }
+
+    r = libusb_bulk_transfer(devh, 0x03, buf, sb.st_size, &wrote, 0);
+    if (r < 0) {
+        fprintf(stderr, "error: libusb_bulk_transfer returned %d\n", r);
+        goto err;
+    }
+    if (wrote != sb.st_size) {
+        fprintf(stderr, "error: size wrote to 2440 not equal to file size\n");
+        goto err;
+    }
+
+    if (buf != NULL)
+        free(buf);
+    if (fd != -1)
+        close(fd);
+    close_s3c2440_usb(devh);
+    return 0;
+
+err:
+    if (buf != NULL)
+        free(buf);
+    if (fd != -1)
+        close(fd);
+    close_s3c2440_usb(devh);
+    return 1;
 }
